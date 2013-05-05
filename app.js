6d7f8bfee2ff70b8fdb85a4053dfd77b6655a0cf
Tim Davies <mail@timdavi.es>
2013-05-05 23:58:27 +0100

    Hooked API file in and added comments, etc

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
+// Get dependencies:
+var express = require('express');
+var http = require('http');
+var path = require('path');
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , http = require('http')
-  , path = require('path');
-
+// Create express app:
 var app = express();
 
-// all environments
+// Define settings and middleware for all environments:
 app.set('port', process.env.PORT || 8000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -18,17 +15,23 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+// Configure development settings and middleware:
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
+// Render single-page app:
 app.get('/', function(req, res) {
-    res.render('app', { title: 'Express' });
+    res.render('app', {});
 });
 
+// API routes:
+require('./api.js')(app);
+
+// Start server:
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
