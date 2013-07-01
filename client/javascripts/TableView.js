a5b89f94f4f5e86837292ecec5bcd77ce1c7e1a1
Tim Davies <mail@timdavi.es>
2013-07-01 01:09:24 +0100

    Add toolbar item

--- client/javascripts/TableView.js
+++ client/javascripts/TableView.js
@@ -17,6 +17,13 @@ var TableView = Backbone.Model.extend({
      * Render content view
      */
     renderContentView: function() {
+        // Clear toolbar and add items:
+        toolbar.clear();
+        toolbar.addItem('left', 'Servers', '', function() {
+            pane.toggle();
+        });
+        
+        // Render HTML:
         $('#main').html(_.template(
             $('#template-tableview').html(),
             {
