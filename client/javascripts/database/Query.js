311dbe08e19fd4355b3db3dbc615980e975db676
Tim Davies <mail@timdavi.es>
2013-07-08 01:20:24 +0100

    Add query class for managing SQL

---
+++ client/javascripts/database/Query.js
@@ -0,0 +1,56 @@
+/**
+ * Holds an SQL query and allows basic manipulation of certain
+ * parameters, such as the LIMIT clause
+ */
+var Query = Backbone.Model.extend({
+    /**
+     * Initialize object with query and begin parsing.
+     *
+     * @param string query SQL statement to parse
+     */
+    initialize: function(query) {
+        this.set('query', query);
+        this.set('limit', 100);
+        this.set('offset', 0);
+    },
+    
+    
+    /**
+     * Set maximum number of results to return
+     *
+     * @param integer limit Maximum number of results
+     *
+     * @return void
+     */
+    setLimit: function (limit) {
+        this.set('limit', limit);
+    },
+    
+    
+    /**
+     * Set result offset for pagination
+     *
+     * @param integer offset Offset for pagination
+     *
+     * @return void
+     */
+    setOffset: function (offset) {
+        this.set('offset', offset);
+    },
+    
+    
+    /**
+     * Convert query into SQL
+     *
+     * @return string SQL
+     */
+    toSQL: function() {
+        var sql = this.get('query');
+        sql += _.str.sprintf(
+            " LIMIT %d, %d",
+            this.get('offset'),
+            this.get('limit')
+        );
+        return sql;
+    }
+});
