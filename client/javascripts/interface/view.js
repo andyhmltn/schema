5ae646369e1a4334e4fc2898e1d687ea18227236
Tim Davies <mail@timdavi.es>
2013-07-10 19:14:22 +0100

    Tweak pane to work properly with new absolute layout

--- client/javascripts/interface/view.js
+++ client/javascripts/interface/view.js
@@ -3,12 +3,8 @@
  */
 var View = Backbone.Model.extend({
     slideAround: function (amount, speed) {
-        $('#main .titlebar').animate({
+        $('#main').animate({
             top: amount + 'px'
-        }, speed);
-        
-        $('#main, #main .tableview').animate({
-            top: (amount + 30) + 'px'
         }, speed);
     },
     
