0c5104741ebaacd7373e34927660da203947ab04
Tim Davies <mail@timdavi.es>
2013-07-11 20:43:58 +0100

    Add JSDoc for slideAround

--- client/javascripts/interface/View.js
+++ client/javascripts/interface/View.js
@@ -2,6 +2,14 @@
  * View
  */
 var View = Backbone.View.extend({
+    /**
+     * Slide the view up or down
+     * 
+     * @param  string amount Distance (pixels) to move view
+     * @param  int    speed  Speed of animation
+     * 
+     * @return void
+     */
     slideAround: function (amount, speed) {
         $('#main').animate({
             top: amount + 'px'
