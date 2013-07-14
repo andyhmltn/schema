027c1eed7a7ad0a693bcffa5df7ce4b7117734c5
Tim Davies <mail@timdavi.es>
2013-07-14 19:45:22 +0100

    Add jsDoc comments to Pane

--- client/javascripts/interface/Pane.js
+++ client/javascripts/interface/Pane.js
@@ -1,10 +1,31 @@
 /**
  * Pane
+ * 
+ * The pane is currently only used for housing the custom query editor,
+ * however could be used for confirmation / extra information dialogues.
+ * 
+ * @class
+ * @author Tim Davies <mail@timdavi.es
  */
 var Pane = Backbone.View.extend({
+    /**
+     * Stores current status of pane
+     * @type {Boolean} True indicates the pane is open
+     */
     isOpen: false,
+    
+    
+    /**
+     * DOM selector for targeting the pane
+     * @type {String}
+     */
     selector: "#pane",
     
+    
+    /**
+     * Open the pane and tell the view to slide down to make room for it.
+     * @return {undefined}
+     */
     open: function() {
         this.isOpen = true;
         
@@ -15,6 +36,11 @@ var Pane = Backbone.View.extend({
         view.slideAround(195, 200);
     },
     
+    
+    /**
+     * Close the pane and tell the view to slide up
+     * @return {undefined}
+     */
     close: function() {
         this.isOpen = false;
         
@@ -25,6 +51,11 @@ var Pane = Backbone.View.extend({
         view.slideAround(45, 200);
     },
     
+    
+    /**
+     * If the pane is open, close it. If it's closed, open it.
+     * @return {undefined}
+     */
     toggle: function() {
         if (this.isOpen) {
             this.close();
@@ -33,7 +64,13 @@ var Pane = Backbone.View.extend({
         }
     },
     
-    render: function(html) {
-        $(this.selector).html(html);
+    
+    /**
+     * Render an object into the pane
+     * @param  {Object} object Object to render, must have a render() method.
+     * @return {undefined}
+     */
+    render: function(object) {
+        $(this.selector).html(object.render());
     }
 });
