b467a52e27b49e837e4f44fd33234919289a4064
Tim Davies <mail@timdavi.es>
2013-07-17 22:09:36 +0100

    Update jsDoc for DatabasePicker

--- client/javascripts/view/DatabasePicker.js
+++ client/javascripts/view/DatabasePicker.js
@@ -1,16 +1,32 @@
+/**
+ * DatabasePicker View
+ * 
+ * @class
+ * @author  Tim Davies <mail@timdavi.es>
+ */
 var DatabasePicker = Backbone.View.extend({
+    /**
+     * Close the pane is it's open
+     * @constructor
+     * @return {undefined}
+     */
     initialize: function() {
         if (pane.isOpen) {
             pane.close();
         }
     },
     
+    
+    /**
+     * Display database picker
+     * @return {undefined}
+     */
     displayPicker: function() {
         // Populate left nav (database switcher):
         database.queryOrLogout("SHOW DATABASES;", function (rows) {
             for (var row_id in rows) {
                 var row = rows[row_id];
-                sidebar.addItem(row.Database, '', '#/database/' + row.Database + '/', 0, undefined, false);
+                sidebar.addItem(row.Database, '', '#/database/' + row.Database + '/', undefined, false);
             }
             sidebar.render();
         });
