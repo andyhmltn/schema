ca469eb0981201f37bc322e4e0475267d6ef2008
Tim Davies <mail@timdavi.es>
2013-07-17 22:12:04 +0100

    Update jsDoc for View

--- client/javascripts/interface/View.js
+++ client/javascripts/interface/View.js
@@ -1,14 +1,16 @@
 /**
  * View
+ * 
+ * @class
+ * @author  Tim Davies <mail@timdavi.es>
  */
 var View = Backbone.View.extend({
     /**
      * Slide the view up or down
-     * 
-     * @param  string amount Distance (pixels) to move view
-     * @param  int    speed  Speed of animation
-     * 
-     * @return void
+     * @param  {String} amount Distance (pixels) to move view. Negative
+     *                         to move up, positive to move down.
+     * @param  {Number} speed  Speed of animation
+     * @return {undefined}
      */
     slideAround: function (amount, speed) {
         $('#main').animate({
