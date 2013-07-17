16bf04ddb59b1aceb0daa9e0b4192ee367039449
Tim Davies <mail@timdavi.es>
2013-07-17 22:11:35 +0100

    Update jsDoc for TableInfo

--- client/javascripts/view/TableInfo.js
+++ client/javascripts/view/TableInfo.js
@@ -1,10 +1,24 @@
+/**
+ * TableInfo View
+ * 
+ * @class
+ * @author  Tim Davies <mail@timdavi.es>
+ */
 var TableInfo = Backbone.View.extend({
+    /**
+     * Initialise object and store table
+     * @param  {Table} table Table to display information about
+     * @constructor
+     * @return {undefined}
+     */
     initialize: function(table) {
         this.table = table;
     },
     
+    
     /**
      * Render view
+     * @return {undefined}
      */
     render: function() {
         var tableview = this;
