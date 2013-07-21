6714aa77807764bfdbb044b14e69b8ebc044b5ab
Tim Davies <mail@timdavi.es>
2013-07-22 00:31:48 +0100

    Add setLoading method to View

--- client/javascripts/interface/View.js
+++ client/javascripts/interface/View.js
@@ -16,5 +16,20 @@ var View = Backbone.View.extend({
         $('#main').animate({
             top: amount + 'px'
         }, speed);
+    },
+    
+    
+    /**
+     * Turn on or off the loading indicator on the content view
+     * 
+     * @param  {Boolean} loading Whether to turn the indicator on or off
+     * @return {undefined}
+     */
+    setLoading: function (loading) {
+        if (loading) {
+            $('#main').addClass('loading');
+        } else {
+            $('#main').removeClass('loading');
+        }
     }
 });
