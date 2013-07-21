51f5898787479d9dc4ae76dc28eb8e91351d7213
Tim Davies <mail@timdavi.es>
2013-07-21 18:19:09 +0100

    Make call to Database::getServerInformation before rendering server report

--- client/javascripts/view/ServerView.js
+++ client/javascripts/view/ServerView.js
@@ -30,13 +30,46 @@ var ServerView = Backbone.View.extend({
             }
             sidebar.render();
             
-            $('#main').html(_.template(
-                $('#template-list-databases').html(),
-                {
-                    server_name: window.server_name,
-                    num_databases: rows.length
+            database.getServerInformation(function(serverInformation) {
+                console.log(serverInformation);
+                
+                // Get the server size:
+                var server_size = parseInt(serverInformation.size);
+                
+                // Default unit is MB:
+                var size_unit = 'MB';
+                
+                // Check for gigabytes:
+                if (server_size >= 1000) {
+                    server_size /= 1000;
+                    size_unit = 'GB';
                 }
-            ));
+                
+                // Check for terabytes:
+                if (server_size >= 1000) {
+                    server_size /= 1000;
+                    size_unit = 'TB';
+                }
+                
+                // Make sure the output is sensible and doesn't look bad
+                // in the circle:
+                if (server_size.toString().indexOf('.') > -1) {
+                    server_size = server_size.toFixed(1)
+                }
+                
+                // Render template:
+                $('#main').html(_.template(
+                    $('#template-list-databases').html(),
+                    {
+                        server_name: window.server_name,
+                        num_databases: rows.length,
+                        size: server_size,
+                        size_unit: size_unit,
+                        version: serverInformation.version,
+                        charset: serverInformation.charset
+                    }
+                ));
+            });
         });
     }
 });
