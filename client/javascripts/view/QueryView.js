dc6caecee737b2cdc450a5a16974603b37d319be
Tim Davies <mail@timdavi.es>
2013-07-24 00:32:59 +0100

    Add QueryView

---
+++ client/javascripts/view/QueryView.js
@@ -0,0 +1,42 @@
+/**
+ * QueryView
+ * 
+ * @author  Tim Davies <mail@timdavi.es>
+ */
+var QueryView = Backbone.View.extend({
+    /**
+     * Initialise object
+     * @return {undefined}
+     */
+    initialize: function() {
+        
+    },
+    
+    
+    /**
+     * Render QueryView
+     * @return {undefined}
+     */
+    render: function() {
+        pane.render();
+        pane.open();
+        
+        $('#main').html(_.template(
+            $('#template-queryview').html(),
+            {
+            }
+        ));
+        
+        this.bindInputs();
+        contentview.setLoading(false);
+    },
+    
+    
+    /**
+     * Bindinputs
+     * @return {undefined}
+     */
+    bindInputs: function() {
+        
+    }
+});
