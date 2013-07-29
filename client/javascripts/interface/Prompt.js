3b927d7aac14addd16e9abcc678552f89e6511fc
Tim Davies <mail@timdavi.es>
2013-07-29 12:11:09 +0100

    Add prompt JS and template

---
+++ client/javascripts/interface/Prompt.js
@@ -0,0 +1,50 @@
+/**
+ * Display a prompt to the user
+ * 
+ * @class
+ * @author  Tim Davies <mail@timdavi.es>
+ */
+var Prompt = Backbone.View.extend({
+    /**
+     * Return itself
+     * @return {Prompt}
+     */
+    initialize: function() {
+        return this;
+    },
+    
+    /**
+     * Display prompt to user and execute callback with results
+     * @param  {String}   message           Message to display above text field
+     * @param  {String}   complete_message  Text to display in 'ok' button
+     * @param  {String}   cancel_message    Text to display in 'cancel' button
+     * @param  {Function} callback Callback to execute with result
+     * @return {undefined}
+     */
+    display: function(message, complete_message, cancel_message, callback) {
+        // Render prompt template:
+        sheet.setTemplate('#template-prompt', {
+            message: message,
+            complete_message: complete_message,
+            cancel_message: cancel_message
+        });
+        
+        // Display prompt:
+        sheet.show();
+        
+        // Select input field:
+        $('#sheet input.prompt').select();
+        
+        // If cancel button is pressed, hide the sheet:
+        $('#sheet .cancel').click(function() {
+            sheet.hide();
+        });
+        
+        // If the 'ok' button is pressed, return the value via callback and
+        // hide the prompt:
+        $('#sheet .complete').click(function() {
+            sheet.hide();
+            callback($('#sheet input.prompt').val());
+        });
+    }
+});
